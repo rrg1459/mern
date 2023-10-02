aebf4ad1a992c9b9b193cec7f0fc08b8082d9a8a
rafaDev <rrg1459@hotmail.com>
2023-10-02 09:09:52 -0400

    Create server express, test route with json response

---
+++ server/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// Load node modules to create server
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Run express (http)
+var app = express();
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//
+app.get('/', (req, res) => {
+  return res.status(200).send({
+    app: 'MERN',
+    author: 'Rafael RodriguezR',
+    url: 'http://localhost:3900/'
+  }
+    
+  );
+});
+
+// Export module (current file)
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 'use strict'
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const app = require('./app');
+const port = 3900;
 
 mongoose.Promise = global.Promise; // Add promise to mongodb
 mongoose.connect('mongodb://localhost:27017/api_rest_blog')
   .then(() => {
     console.log('Connection successfully!!!');
+
+    // Create server and listen to HTTP requests
+    app.listen(port, () => {
+      console.log('Server running on http://localhost:' + port);
+    });
   });
