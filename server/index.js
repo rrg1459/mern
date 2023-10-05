1457082812e892a706cf95e1e3b947522d5e563c
rafaDev <rrg1459@hotmail.com>
2023-10-05 11:13:28 -0400

    Check mongodb service is alive

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+console.log('\nDid you check the mongodb service?  -->  $  systemctl status mongod\n');
+
 const mongoose = require('mongoose');
 const app = require('./app');
 const port = 3900;
@@ -7,10 +9,14 @@ const port = 3900;
 mongoose.Promise = global.Promise; // Add promise to mongodb
 mongoose.connect('mongodb://localhost:27017/api_rest_blog')
   .then(() => {
+    console.log('Mongodb service is running');
     console.log('Connection successfully!!!');
 
     // Create server and listen to HTTP requests
     app.listen(port, () => {
-      console.log('Server running on http://localhost:' + port);
+      console.log('Server running on http://localhost:' + port + '\n');
     });
+  })
+  .catch(() => {
+    console.error('Please start mongod service  --> $  sudo systemctl start mongod\n');
   });
