ce03d35f8e05b616237a279ef883b07c923a12ec
rafaDev <rrg1459@hotmail.com>
2023-10-09 10:04:41 -0400

    Slider component

---
+++ client/src/components/Slider.js
@@ -0,0 +1,10 @@
+const Slider = () => {
+
+  return (
+    <div id="slider" class="slider-big">
+    <h1>Welcome to MERN app</h1>
+    <a href="/" class="btn-white">Go to blog</a>
+</div>
+  );
+}
+export default Slider;
