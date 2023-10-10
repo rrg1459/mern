56f33810ca1dcd404cbaf38f66cb1a892676d94c
rafaDev <rrg1459@hotmail.com>
2023-10-10 11:58:02 -0400

    Modify an array attribute with useState

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
+
 const Movies = () => {
 
   const name = "Rafael";
@@ -11,10 +12,15 @@ const Movies = () => {
     setMovies([
       { title: 'Batman vs Superman', year: 2017, image: "https://cde.laprensa.e3.pe/ima/0/0/1/2/8/128722.jpg" },
       { title: 'Gran torino', year: 2015, image: "https://www.marquid.com/wp-content/uploads/2015/05/gran-torino.jpg" },
-      { title: 'The Lord of the rings', year: 2003, image: "https://sfo2.digitaloceanspaces.com/estaticos/var/www/html/wp-content/uploads/2019/07/El-sen%CC%83or-de-los-anillos.jpg" },
+      { title: 'The Lord of the rings', year: 2003, image: "https://sfo2.digitaloceanspaces.com/estaticos/var/www/html/wp-content/uploads/2019/07/El-sen%CC%83or-de-los-anillos.jpg" }
     ])
   }, [])
 
+  const changeTitle = () => {
+    const random = Math.floor(Math.random() * 3);
+    movies[random].title = 'Title changed';
+    setMovies([...movies]);
+  }
 
   return (
     <div id="content" className="movies">
@@ -22,11 +28,11 @@ const Movies = () => {
       <h2 className="subheader">Movies</h2>
       <p>Selection of favorite movies of: {name}</p>
 
+      <p><button onClick={changeTitle}>Change title</button></p>
+
       <div id="articles" className="movies">
         { movies.map((movie, i) => <Movie movie={movie} key={i} /> ) }
       </div>
-
-
 
     </div>
   );
