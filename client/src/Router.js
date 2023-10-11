e5cb932ebb336b6b6771abcfa6528cf463568418
rafaDev <rrg1459@hotmail.com>
2023-10-11 08:27:52 -0400

    Route 404

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,23 +4,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TestSection from './components/TestSection';
 import MyComponent from './components/MyComponent';
 import Movies from './components/Movies';
+import Error from './components/Error';
 
 const Router = () => {
 
-    return (
+  return (
 
-      <BrowserRouter>
+    <BrowserRouter>
 
-        <Routes>
+      <Routes>
 
-          <Route path="/" element={<Movies />} />
-          <Route path="/test" element={<TestSection />} />
-          <Route path="/second" element={<MyComponent />} />
+        <Route path="/" element={<Movies />} />
+        <Route path="/test" element={<TestSection />} />
+        <Route path="/second" element={<MyComponent />} />
+        <Route path="*" element={<Error />} />
 
-        </Routes>
+      </Routes>
 
-      </BrowserRouter>
-    )
+    </BrowserRouter>
+  )
 
 }
 
