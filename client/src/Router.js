63c74556e969a800b87ee3a4b2d442fa693d2d4c
rafaDev <rrg1459@hotmail.com>
2023-10-11 08:29:16 -0400

    Route without element and with a component with parameter

--- client/src/Router.js
+++ client/src/Router.js
@@ -17,6 +17,12 @@ const Router = () => {
         <Route path="/" element={<Movies />} />
         <Route path="/test" element={<TestSection />} />
         <Route path="/second" element={<MyComponent />} />
+        <Route path="/no-component" element={(
+          <>
+            <h1>From the page No Component</h1>
+            <MyComponent greetings="Hi folk" />
+          </>
+        )} />
         <Route path="*" element={<Error />} />
 
       </Routes>
