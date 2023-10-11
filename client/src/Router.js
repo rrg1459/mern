bbfa639165e09182becd4d3951f4b42da8a76226
rafaDev <rrg1459@hotmail.com>
2023-10-11 07:37:55 -0400

    Route configuration

---
+++ client/src/Router.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import TestSection from './components/TestSection';
+import MyComponent from './components/MyComponent';
+import Movies from './components/Movies';
+
+class Router extends Component {
+
+  render() {
+    return (
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path="/" element={<Movies />} />
+          <Route path="/test" element={<TestSection />} />
+          <Route path="/second" element={<MyComponent />} />
+
+        </Routes>
+
+      </BrowserRouter>
+    );
+  }
+
+}
+
+export default Router;
