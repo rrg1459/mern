a27fcd599a5c1554c1553fd6e698fb56a1354cb4
rafaDev <rrg1459@hotmail.com>
2023-10-13 10:36:26 -0400

    First steps with axios

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Sidebar from "./Sidebar";
 import Slider from "./Slider";
 
 const Blog = () => {
+
+  const [articles, setArticles] = useState({});
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:3900/api/articles")
+      .then(res => {
+        setArticles(res.data.articles);
+        setStatus('success');
+        console.log('xxx res.data-->: ', res.data);
+      });
+  }, [])
 
   return (
     <div id="blog">
@@ -12,6 +26,14 @@ const Blog = () => {
       <div className="center">
         <div id="content">
           {/* ***  */}
+          {status === 'success' &&
+            <div>
+              {articles.map((article) => {
+                return (
+                  <h1 key={article._id}>{article.title}</h1>
+                  )
+              })}
+            </div>}
         </div>
 
         <Sidebar
