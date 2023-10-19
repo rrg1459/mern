ae24aad53cfaa46a3fbb6ae165c123c1fa1ac49b
rafaDev <rrg1459@hotmail.com>
2023-10-19 09:26:24 -0400

    Add article data and format css

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,41 +1,88 @@
-// import axios from 'axios';
-// import Global from '../Global';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Global from '../Global';
 import Sidebar from './Sidebar';
+import { useParams } from 'react-router-dom';
+import Moment from 'react-moment';
+import imageDefault from '../assets/images/default.png'
 // import { Link, Navigate } from 'react-router-dom';
 
-
 const Article = () => {
+
+  const [article, setArticle] = useState(false);
+  const [status, setStatus] = useState(null);
+  const url = Global.url;
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      setTimeout(() => {
+        axios.get(url + "article/" + id)
+          .then(res => {
+            setArticle(res.data.article);
+            setStatus('success');
+          })
+          .catch(err => {
+            setArticle(false);
+            setStatus('success');
+          });
+      }, 1000)
+    }
+  }, [id, url])
+
+  console.log('xxx article-->: ', article);
+
   return (
     <div className="center">
       <section id="content">
-
-        <article className="article-item article-detail">
-          <div className="image-wrap">
-            <img src="https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg" alt="Paisaje" />
+        {!article && !status &&
+          <div id="articles">
+            <h2 className="subheader">Loading...</h2>
+            <p>Wait while the content is loading</p>
           </div>
+        }
+        {article &&
+          <article className="article-item article-detail">
 
-          <h1 className="subheader">Test article</h1>
-          <span className="date">
-            5 minutes ago
-          </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consectetur dui. Vestibulum ac convallis urna, vitae porta massa. Mauris sit amet nisi in metus tempor convallis. Nulla nec euismod turpis. Cras luctus lorem et nisl dapibus aliquet. Curabitur lorem nunc, tristique a felis ac, vehicula laoreet ante. Ut auctor orci turpis. Cras sit amet placerat nulla, feugiat eleifend metus. Mauris nec convallis lectus. In gravida sapien in iaculis vulputate. Aliquam a rhoncus elit, sit amet pretium nisl. Vivamus egestas facilisis viverra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vel lorem est. Cras et sapien finibus, mattis est in, condimentum nisl.
-          </p>
-          <p>
-            Aenean vel orci a tellus porttitor eleifend. Integer tincidunt porta fermentum. Aenean vitae enim iaculis, sollicitudin risus in, interdum justo. Donec semper metus ac nibh maximus venenatis. Cras sodales nisl metus, a placerat risus tristique ornare. Ut finibus nisi a ante tincidunt hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut eget venenatis augue, id molestie eros. Quisque leo risus, pellentesque mattis libero id, pharetra tempor turpis.
-          </p>
-          <p>
-            Donec fermentum ligula at ultrices faucibus. Proin facilisis est vitae vehicula finibus. Praesent rutrum eleifend ligula, sit amet molestie dolor interdum nec. Cras sodales odio nec diam posuere, mollis aliquam magna tempus. Fusce lobortis maximus dapibus. Curabitur aliquam vehicula ultricies. Nulla sed vulputate erat.
-          </p>
+            <div className="">
+              {article.image ? (
+                <div className="image-wrap">
+                  <img src={url + 'get-image/' + article.image} alt={article.title} />
+                </div>
+              ) : (
+                <div className="image-wrap">
+                  <img src={imageDefault} alt={article.title} />
+                </div>
+              )
+              }
+            </div>
 
-          <div className="clearfix"></div>
-        </article>
 
+            <h1 className="subheader">{article.title}</h1>
+            <span className="date">
+              <Moment fromNow>{article.date}</Moment>
+            </span>
+
+            <p>{article.content}</p>
+
+            <a href="/" className="btn btn-danger">Delete</a>
+            <a href="/" className="btn btn-warning">Edit</a>
+
+
+            <div className="clearfix"></div>
+          </article>
+        }
+        {
+          !article && status === 'success' &&
+          <div id="articles">
+            <h1>No article to show</h1>
+          </div>
+        }
       </section>
 
       <Sidebar />
-
       <div className="clearfix"></div>
+
     </div>
   )
 }
