fba105de6dc65936cefb5a283833df09be2170d9
rafaDev <rrg1459@hotmail.com>
2023-10-20 08:16:17 -0400

    Add form to create article

--- client/src/components/CreateArticle.js
+++ client/src/components/CreateArticle.js
@@ -5,6 +5,24 @@ const CreateArticle = () => {
     <div className="center">
       <section id="content">
         <h1 className="subheader">Create Article</h1>
+
+        <form className="mid-form">
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">Content</label>
+            <textarea name="content" ></textarea>
+          </div>
+          <div className="form-group">
+            <label htmlFor="img">Image</label>
+            <input type="file" name="img" ></input>
+          </div>
+          <div className="clearfix"></div>
+          <input type="submit" value="Save" className="btn btn-success" />
+        </form>
+
       </section>
       <Sidebar />
     </div>
