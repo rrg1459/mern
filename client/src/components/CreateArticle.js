0653bf9604cd250b051dce19f5e1e76cf0d3cbf7
rafaDev <rrg1459@hotmail.com>
2023-10-20 08:00:30 -0400

    Add CreateArticle component

---
+++ client/src/components/CreateArticle.js
@@ -0,0 +1,14 @@
+import Sidebar from "./Sidebar"
+
+const CreateArticle = () => {
+  return (
+    <div className="center">
+      <section id="content">
+        <h1 className="subheader">Create Article</h1>
+      </section>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default CreateArticle
